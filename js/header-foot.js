dc473d8946104d4c622d8158c2fe52de658c9b1f
suningun <suningun55555@gmail.com>
2025-08-06 15:41:44 +0700

    all

---
+++ js/header-foot.js
@@ -0,0 +1,77 @@
+let header=`
+                <div class="container-fluid bg-light p-3 sticky-top" style="width: 100%;">
+                <a class="navbar-brand" href="index.html">
+                    <i class="bi bi-emoji-sunglasses"></i>
+                    <span class="fw-semibold">Suning Un</span>
+                </a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link fw-semibold" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item dropdown fw-semibold"><a class="nav-link dropdown-toggle" href="" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">Resume</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="volunteering.html">Volunteering</a></li>
+                        <li><a class="dropdown-item" href="achievements.html">Achievements</a></li>
+                        <li><a class="dropdown-item" href="educations.html">Educations</a></li>
+                        <li><a class="dropdown-item" href="experiences.html">Experiences</a></li>
+                        <li><a class="dropdown-item" href="skills.html">Skills</a></li>
+                    </ul>
+                    <li class="nav-item">
+                        <a class="nav-link fw-semibold" href="about-me.html">About Me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-semibold" href="contact.html">Contact</a>
+                    </li>
+                    </ul>
+                </div>
+            </div>`;
+
+document.getElementById("header").innerHTML = header;
+
+let footer = `<div class="container-fluid bg-light">
+        <div class=" container-fluid py-4 px-5">
+        <div class="row">
+            <div class="col-sm-5 mb-5 mb-sm-0 contact-info">
+                <p class="fw-bold fs-5">About Me</p>
+                <p class="text-muted">Thank you for visiting my personal portfolio website. Connect with me over socials.</p>
+                <p class="text-muted">Keep Rising 🚀. Connect with me over live chat!</p>
+            </div>
+            <div class="col-sm-4 mb-4 mb-sm-0 contact-info">
+                <p class="fw-bold fs-5">Contact</p>
+                <div class="footer-icons">
+                    <div class="d-flex align-items-center mb-1"><i class="bi bi-telephone me-2"></i><span>   070833235 </span></div>
+                    <div class="d-flex align-items-center mb-1"><i class="bi bi-envelope me-2"></i><span>   suningun55555@gmail.com</span></div>
+                    <div class="d-flex align-items-center mb-1"><i class="bi bi-geo-alt me-2"></i><span>   Phnom Penh City</span></div>
+            </div>
+            </div>
+
+            <div class="col-sm-3 mb-3 mb-sm-0 text-sm-start contact-info">
+                <p class="fw-bold fs-5">Follow</p>
+                <div class="footer-icons d-flex justify-content-sm-start fs-4">
+                    <a href="https://github.com/suningun" target="_blank"><i class="bi bi-github"></i></a>
+                    <a href="https://web.facebook.com/suning0.0" target="_blank"><i class="bi bi-facebook"></i></a>
+                    <a href="https://www.linkedin.com/in/suning-un-85a439344/" target="_blank"><i class="bi bi-linkedin"></i></a>
+                    <a href="https://www.instagram.com/suning_un/" target="_blank"><i class="bi bi-instagram"></i></a>
+                    <a href="https://x.com/ing_un66197" target="_blank"><i class="bi bi-twitter"></i></a>
+                </div>
+            </div>
+            </div>
+            <div class="col-12 d-flex justify-content-center text-center contact-info fw-semibold">© 2025 by Suning Un. All Rights Reserved</div>
+        </div>
+    </div>`;
+
+document.getElementById("footer").innerHTML = footer;
+
---
+++ js/index.js
@@ -0,0 +1,47 @@
+    const skill = [
+    { image: "images/html-5.png", title: "HTML" },
+    { image: "images/css-3.png", title: "CSS" },
+    { image: "images/js.png", title: "JavaScript" },
+    { image: "images/java.png", title: "Java" },
+    { image: "images/python.png", title: "Python" },
+    { image: "images/github.png", title: "Github" },
+    ];
+
+    let content = "";
+    for (let i = 0; i < skill.length; i++) {
+    content += `
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2 d-flex justify-content-center mb-4">
+        <div class="card" style="height: 100px; width: 100px; overflow: hidden;">
+            <div class="card-body text-center d-flex flex-column align-items-center justify-content-center p-2">
+            <div class="img-container mb-2">
+                <img src="${skill[i].image}" width="50" height="50" alt="${skill[i].title}" style="max-width: 100%; height: auto;">
+            </div>
+            <p class="fs-6 mb-0">${skill[i].title}</p>
+            </div>
+        </div>
+        </div>`;
+    }
+
+    document.getElementById("skill").innerHTML = content;
+
+    
+    
+    const education = [
+    { image: "images/aupp.png", major: "Bachelor of Science In Software Development", institution: "American University Of Phnom Penh | AUPP", date: "2023" },
+    { image: "images/soc.png", major: "Coding Program", institution: "Sisters Of Code", date: "2021-2025" },
+    { image: "images/crp.jpg", major: "Grade 7-12", institution: "Chamrouen Phal High School", date: "2018-2024" },
+    { image: "images/fotang.jpg", major: "Chinese Language", institution: "Guang Chao Fo Tang (僙超佛堂)", date: "2018-2023"},
+    ]
+    let educationContent = "";
+    for (let i = 0; i < education.length; i++) {
+    educationContent += `<div class="container-fluid mb-4 row shadow-sm mx-auto" style="width: 80%; height: auto;">
+                            <div class="col-md-4 mb-md-0 text-center mx-auto"><img class="border-circle" src="${education[i].image}" width="150" height="150" style="max-width: 100%; height: auto;"></div>
+
+                            <div class="col-md-8 mb-md-0 py-4">
+                                <h4>${education[i].major}</h4>
+                                <p>${education[i].institution}</p>
+                                <h5>${education[i].date}</h5>
+                            </div>
+                        </div>`;
+    }
+    document.getElementById("education").innerHTML = educationContent;
